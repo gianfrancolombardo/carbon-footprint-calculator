5d47fdfbbc3894c33469c0cf7cd7bbd62c5049af
gianfrancolombardo <gianf.lombardo@gmail.com>
2022-02-22 17:21:07 +0100

    delete unnecessary files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB1WJUEy7a-DvhjxJJcn8k64m6NWDC5hD4",
+    authDomain: "carbon-footprint-6c319.firebaseapp.com",
+    projectId: "carbon-footprint-6c319",
+    storageBucket: "carbon-footprint-6c319.appspot.com",
+    messagingSenderId: "557798348118",
+    appId: "1:557798348118:web:ca4d25e38b84770d4472f7",
+    measurementId: "G-46QS24TJEX"
+  }
 };
